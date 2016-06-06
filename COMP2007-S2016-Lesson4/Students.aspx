2618342c5117703c2364a8ac6b437779bc3436be
Wangzhetuteng <ericdicaprio@outlook.com>
2016-06-06 11:19:47 -0400

    Added GridView to Students page and connected to db.

--- COMP2007-S2016-Lesson4/Students.aspx
+++ COMP2007-S2016-Lesson4/Students.aspx
@@ -1,6 +1,18 @@
 ﻿<%@ Page Title="Students" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Students.aspx.cs" Inherits="COMP2007_S2016_Lesson4.About1" %>
+
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
     <div class="container">
-        <h1>Students Page</h1>
+        <div class="row">
+            <div class="col-md-offset-2 col-md-8">
+                <h1>Student List</h1>
+                <asp:GridView runat="server" CssClass="table table-bordered table-striped table-hover" 
+                    ID="StudentsGridView" AutoGenerateCollumns="false">
+                    <Columns>
+                                       
+                    </Columns>
+                </asp:GridView>
+            </div>
+        </div>
     </div>
+
 </asp:Content>
--- COMP2007-S2016-Lesson4/Students.aspx.cs
+++ COMP2007-S2016-Lesson4/Students.aspx.cs
@@ -5,13 +5,47 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
+// using statements that are required to connect to EF DB
+using COMP2007_S2016_Lesson4.Models;
+using System.Web.ModelBinding;
+
+
 namespace COMP2007_S2016_Lesson4
 {
     public partial class About1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //if loading the page for the first time, populate the student grid
+            if(!IsPostBack)
+            {
+                //get the student data
+                this.GetStudents();
+            }
+        }
+        
+        /**
+         *<summary>
+         * This method gets the students data from the DB
+         *</summary>
+         * 
+         * @method GetStudents
+         * @returns {void}
+         */
+        protected void GetStudents()
+        {
+            // connect to EF
+            using (DefaultConnectionYd db = new DefaultConnectionYd())
+            {
+                // query the Students Table using EF and LINQ
+                var Students = (from allStudents in db.Students
+                                select allStudents);
+                //bind the result to the GridView
+                StudentsGridView.DataSource = Students.ToList();
+                StudentsGridView.DataBind();
 
+            }
         }
     }
 }
